c1fc51e789ff4d8bbebc626c565973dc56181122
Busra Turkoglu <bturkoglu9393@gmail.com>
2022-11-01 15:44:57 +0300

    01.11

---
+++ Matematiksel Fonksiyonlar.sql
@@ -0,0 +1,46 @@
+--Select (-5)
+--Select abs(-5)  
+--"abs" sayinin mutlak degerini almak icin kullanilir
+
+--select (4.85)
+--select ceil(4.85)
+--ceil veya ceilling  sayiyi en yakin onluga yuvarlamak icin kullanilir.
+
+--select  floor(5.85)
+--sayiyi bit onceki onluga yuvarlamak icin kullanilir
+
+--select pi()
+
+--select power(2,4)
+--ust alma islemlerinde power kullanilir
+
+
+--select Random()
+--0 ile 1 arasi ratgele ondalik bir deger getirir.
+
+--select Round(18.12345,2)
+--virgulden sora kac basamak geleegini belirtmek icin kullanilir
+
+--select sign(-25)
+--girilen sayinin negatif pozitif ve sifir olmasi durumunda -1,1 ve 0 dondurur
+
+--select Sqrt(625)
+--karakok almak icin kullanilir.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
